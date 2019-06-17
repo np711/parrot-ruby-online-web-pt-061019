2f21e8e8d1758a50835f9c72c9e9fa6fc54a7774
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-06-17 18:06:57 +0000

    Automatically backed up by Learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -1,5 +1,6 @@
 require_relative './spec_helper'
-require_relative '../parrot.rb' # Code your solution in this file
+require_relative '../parrot.rb' #parrot
+#puts "Squawk!"
 
 describe '#parrot' do
   it 'should output "Squawk!" to the terminal when called without any arguments' do
